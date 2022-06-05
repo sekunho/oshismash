f2992e432f368581d34aeef2f11ce364b71dbf62
SEKUN <sekun@hey.com>
2022-06-05 20:18:22 +0800

    docs: Add todos

--- src/lib.rs
+++ src/lib.rs
@@ -19,15 +19,18 @@ pub async fn run(db_handle: db::Handle) -> Result<(), hyper::Error> {
     let db_handle = Arc::new(db_handle);
 
     // TODO: Add cookie stuff to middleware
+    // TODO: Add rate limiter
     let middleware = ServiceBuilder::new().layer(AddExtensionLayer::new(db_handle));
 
+    // TODO: Implement dynamic version of UI (JS)
     let app = Router::new()
         .route("/", routing::get(handlers::vtuber::show_from_cookie))
         .route("/", routing::post(handlers::vote::vote))
         // .route("/rpc/vote", routing::post(handlers::vote::rpc_vote))
         .merge(SpaRouter::new("/assets", "public"))
-        // TODO: REMOVE THIS FOR AN ACTUAL FAVICON
+        // TODO: Remove this for an actual favicon
         .route("/favicon.ico", routing::get(handlers::vtuber::touch_grass))
+        // TODO: Maybe it'll be better to move this to `/` instead.
         .route("/touch-grass", routing::get(handlers::vtuber::touch_grass))
         .route("/:vtuber_id", routing::get(handlers::vtuber::show_given_id))
         .layer(middleware.into_inner());
